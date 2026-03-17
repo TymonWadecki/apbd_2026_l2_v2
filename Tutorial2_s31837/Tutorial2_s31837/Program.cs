10b4b327d6a5aa01e2e4074e6ddd81416fe08900
Tymon Wadecki <s31837@pjwstk.edu.pl>
2026-03-17 23:04:12 +0100

    Zmiana deklaracji nazwy metody GetAverage na CalculateAverage oraz modfikacja wywołania dla zmiennej avg w  w feature-average branch

--- Tutorial2_s31837/Tutorial2_s31837/Program.cs
+++ Tutorial2_s31837/Tutorial2_s31837/Program.cs
@@ -1,9 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 int [] numbers = {1,2,3,4,5,10};
-static double GetAverage(int[] values) => values.Average();
+static double CalculateAverage(int[] values) => values.Average();
 
-double avg = GetAverage(numbers);
+double avg = CalculateAverage(numbers);
 Console.WriteLine($"Average value is: {avg}");
 
 static int GetMax(int[] values) => values.Max();
