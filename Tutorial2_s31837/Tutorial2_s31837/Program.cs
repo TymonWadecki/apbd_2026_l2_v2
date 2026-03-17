8d82c7a5f0fe2409d9c5015e00143a8f52288a20
Tymon Wadecki <s31837@pjwstk.edu.pl>
2026-03-17 23:26:07 +0100

    Zmiania nazwy metody wyliczajacej maksymalną wartość oraz usunięcie komentarza zrobionego dla gałęzi feature-ff-correct

--- Tutorial2_s31837/Tutorial2_s31837/Program.cs
+++ Tutorial2_s31837/Tutorial2_s31837/Program.cs
@@ -6,6 +6,6 @@ static double CalculateAverage(int[] values) => values.Average();
 double avg = CalculateAverage(numbers);
 Console.WriteLine($"Average value is: {avg}");
 
-static int GetMax(int[] values) => values.Max();
-Console.WriteLine($"Max value is: {GetMax(numbers)}");
+static int CalculateMax(int[] values) => values.Max();
+Console.WriteLine($"Max value is: {CalculateMax(numbers)}");
 //przepraszam ale niechcacy zrobilem merge bez fast forward wiec robie wersje prawidlowa 
