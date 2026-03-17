e965047d495d3ce6a3ecc7ae72cb05d0a232bb7a
Tymon Wadecki <s31837@pjwstk.edu.pl>
2026-03-17 23:18:12 +0100

    dodanie komentarza wyjasniajacego co poszło nie tak

--- Tutorial2_s31837/Tutorial2_s31837/Program.cs
+++ Tutorial2_s31837/Tutorial2_s31837/Program.cs
@@ -8,3 +8,4 @@ Console.WriteLine($"Average value is: {avg}");
 
 static int GetMax(int[] values) => values.Max();
 Console.WriteLine($"Max value is: {GetMax(numbers)}");
+//przepraszam ale niechcacy zrobilem merge bez fast forward wiec robie wersje prawidlowa 
