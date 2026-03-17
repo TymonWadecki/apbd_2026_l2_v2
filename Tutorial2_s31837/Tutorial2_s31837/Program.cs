7c3122bac6aae93d9655c28e8d75505a5b990001
Tymon Wadecki <s31837@pjwstk.edu.pl>
2026-03-17 23:49:22 +0100

    dodanie wyswietlenia na konosli tesktu ktory w przyszlosci bedzie zmieniany

--- Tutorial2_s31837/Tutorial2_s31837/Program.cs
+++ Tutorial2_s31837/Tutorial2_s31837/Program.cs
@@ -17,3 +17,4 @@ Console.WriteLine($"Max value is: {CalculateMax(numbers)}");
 
 static int GetMin(int[] values) => values.Min();
 Console.WriteLine($"Min: {GetMin(numbers)}");
+Console.WriteLine("Tekst do zmiany");
